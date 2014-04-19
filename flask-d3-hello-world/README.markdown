e048402acf5cb25f58514064c5e675bf4ef8833d
Tim Brown <tbrown126@Tims-Mac.local>
2014-04-19 16:29:35 -0700

    flask and d3js example

---
+++ flask-d3-hello-world/README.markdown
@@ -0,0 +1,20 @@
+# flask+d3 for TheoryAndPractice.org
+
+### author
+
+* [Kyle Cranmer](http://theoryandpractice.org/)
+
+
+### contributors
+
+Adapted from [flask-d3-hello-world](https://github.com/dfm/flask-d3-hello-world)
+* [Dan Foreman-Mackey](http://danfm.ca/)
+* [David W. Hogg](http://cosmo.nyu.edu/hogg/)
+
+### do it
+
+1. Install [flask](http://flask.pocoo.org/) and [numpy](http://numpy.scipy.org/).
+
+2. Execute:
+
+    python app.py
---
+++ flask-d3-hello-world/app.py
@@ -0,0 +1,93 @@
+"""
+This file is part of the flask+d3 Hello World project.
+"""
+import json
+import flask
+from flask import request
+import numpy as np
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/hello")
+def index():
+    """
+    When you request the root path, you'll get the index.html template.
+
+    """
+    return flask.render_template("index.html")
+
+
+@app.route("/")
+def gindex():
+    """
+    When you request the gaus path, you'll get the gaus.html template.
+
+    """
+    mux = request.args.get('mux', '')
+    muy = request.args.get('muy', '')
+    if len(mux)==0: mux="3."
+    if len(muy)==0: muy="3."
+    return flask.render_template("gaus.html",mux=mux,muy=muy)
+
+
+@app.route("/data")
+@app.route("/data/<int:ndata>")
+def data(ndata=100):
+    """
+    On request, this returns a list of ``ndata`` randomly made data points.
+
+    :param ndata: (optional)
+        The number of data points to return.
+
+    :returns data:
+        A JSON string of ``ndata`` data points.
+
+    """
+    x = 10 * np.random.rand(ndata) - 5
+    y = 0.5 * x + 0.5 * np.random.randn(ndata)
+    A = 10. ** np.random.rand(ndata)
+    c = np.random.rand(ndata)
+    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
+        "color": c[i]}
+        for i in range(ndata)])
+
+@app.route("/gdata")
+@app.route("/gdata/<float:mux>/<float:muy>")
+def gdata(ndata=100,mux=.5,muy=0.5):
+    """
+    On request, this returns a list of ``ndata`` randomly made data points.
+    about the mean mux,muy
+
+    :param ndata: (optional)
+        The number of data points to return.
+
+    :returns data:
+        A JSON string of ``ndata`` data points.
+
+    """
+
+    """Instead of getting values from URL, try from request object"""
+    mux = request.args.get('mux', '')
+    muy = request.args.get('muy', '')
+
+    x = np.random.normal(mux,.5,ndata)
+    y = np.random.normal(muy,.5,ndata)
+    A = 10. ** np.random.rand(ndata)
+    c = np.random.rand(ndata)
+    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
+        "color": c[i]}
+        for i in range(ndata)])
+
+if __name__ == "__main__":
+    import os
+
+    port = 8000
+
+    # Open a web browser pointing at the app.
+    os.system("open http://localhost:{0}/".format(port))
+
+    # Set up the development server on port 8000.
+    app.debug = True
+    app.run(port=port)
